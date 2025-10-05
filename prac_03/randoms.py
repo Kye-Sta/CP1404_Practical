6d6351e2a484dab44f09fe72003f7bbf9bee1691
Kye-Sta <kye.stanley@my.jcu.edu.au>
2025-10-05 18:03:45 +1000

    Add Prac 3 Files

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+"""
+Line 1: 14
+    - smallest number was 5
+    - largest number was 20
+Line 2: 9
+    -smallest number was 3
+    -largest number was 9
+    -No, a four could not be produced
+Line 3: 4.598 (Rounded)
+    -smallest number was 2.5
+    -largest number was 5.5
+"""
+
+import random
+print(random.randint(1,100))
