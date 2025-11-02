e4851bdbed0106afbf91d473ca51229af1935a85
Kye-Sta <kye.stanley@my.jcu.edu.au>
2025-11-02 19:39:20 +1000

    Fixed Error, found in guitar.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -9,12 +9,11 @@ def main():
     cost = 16035.40
 
     guitar = Guitar(name, year, cost)
-    Another = Guitar("Another Guitar", 2012, 1512.9)
+    another = Guitar("Another Guitar", 2012, 1512.9)
 
-    print(f"{guitar.name} get_age() - Expected {95}. Got {guitar.get_age()}")
-    print(f"{Another.name} get_age() - Expected {5}. Got {Another.get_age()}")
-    print()
+    print(f"{guitar.name} get_age() - Expected {103}. Got {guitar.get_age()}")
+    print(f"{another.name} get_age() - Expected {13}. Got {another.get_age()}")
     print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
-    print(f"{Another.name} is_vintage() - Expected {False}. Got {Another.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected {False}. Got {another.is_vintage()}")
 
 main()
