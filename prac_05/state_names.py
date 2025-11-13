c77a6372446232171ed92e23f46f1a2299631374
Kye-Sta <kye.stanley@my.jcu.edu.au>
2025-11-13 21:45:02 +1000

    Add: loop to print all states with neat formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,3 +16,6 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+
+for state, state_code in CODE_TO_NAME.items():
+    print(f"{state:3} is {state_code}")
