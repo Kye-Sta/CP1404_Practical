dd21709c19bf9428ad0dfbbd731a84d08724a859
Kye-Sta <kye.stanley@my.jcu.edu.au>
2025-11-13 21:49:24 +1000

    Add: Colours and corresponding hex code dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+
+#Dictionary of basic color and their corresponding hex codes
+COLOUR_CODES = {"Black": "#000000", "White": "#ffffff", "Red": "#ff0000", "Green": "#008000",
+"Blue": "#0000ff", "Yellow": "#ffff00", "Cyan": "#00ffff", "Magenta": "#ff00ff", "Gray": "#808080",
+"Orange": "#ffa500"}
