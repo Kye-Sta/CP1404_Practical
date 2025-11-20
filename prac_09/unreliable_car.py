60ba420fe9553c706055138086dc9d518b7aa52a
Kye-Sta <kye.stanley@my.jcu.edu.au>
2025-11-20 22:29:05 +1000

    Add: boolean movement to determine results of reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -7,7 +7,7 @@ from random import randint
 from prac_09.car import Car
 
 class UnreliableCar(Car):
-    def __init(self, name, fuel, reliability):
+    def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
 
@@ -16,4 +16,4 @@ class UnreliableCar(Car):
         if ran_int >= self.reliability:
             distance = 0
         distance_driven = super().drive(distance)
-        return distance_driven
+        return distance_driven > 0
